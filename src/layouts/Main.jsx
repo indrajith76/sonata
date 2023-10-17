cc975107cf1fcac69fdd32cced97a39cf67665f6
indrajith76 <indrajithgoswami@gmail.com>
2023-10-17 18:42:54 +0600

    added sidebar

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,11 @@
-import { Outlet } from "react-router-dom";
-import { BiSearch, BiSolidCircle } from "react-icons/bi";
+import { Outlet, Link } from "react-router-dom";
+import {
+  BiSearch,
+  BiSolidCircle,
+  BiLogoTwitter,
+  BiLogoTelegram,
+  BiLogoDiscordAlt,
+} from "react-icons/bi";
 
 const Main = () => {
   return (
@@ -124,9 +130,77 @@ const Main = () => {
         </ul>
       </div>
 
-      <div>
-        <div></div>
-      <Outlet />
+      <div className="flex">
+        <div className="w-[280px] bg-slate-900 flex flex-col justify-between"> 
+          <div>
+            <ul className="my-8 pl-4 flex flex-col gap-8">
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Tokens
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Wallet
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Presales
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Staking & Farms
+                </Link>
+              </li>
+            </ul>
+            <p className="text-slate-500 text-xs pl-4">Tools for Creators</p>
+            <ul className="my-8 pl-4 flex flex-col gap-8">
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Create a Token
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white text-sm" to="/">
+                  Start a Presale
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="ml-4 mb-8">
+            <p className="text-white text-xs flex items-center gap-1 mb-4">
+              <BiSolidCircle className="text-[4px]" />
+              CONNECTED
+            </p>
+            <ul className="flex gap-5">
+              <li className="bg-slate-800 w-8 h-8 flex justify-center items-center rounded-md">
+                <Link to="" className="text-sm text-white">
+                  <BiLogoTwitter />
+                </Link>
+              </li>
+              <li className="bg-slate-800 w-8 h-8 flex justify-center items-center rounded-md">
+                <Link to="" className="text-sm text-white">
+                  <BiLogoDiscordAlt />
+                </Link>
+              </li>
+              <li className="bg-slate-800 w-8 h-8 flex justify-center items-center rounded-md">
+                <Link to="" className="text-sm text-white">
+                  <BiLogoTelegram />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="border w-full">
+          <Outlet />
+        </div>
       </div>
     </section>
   );
